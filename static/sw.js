e2e47c694f20229ae447e34d9e119382fc764543
songbeomjun <sonalskdjfhfg@gmail.com>
2026-02-26 16:11:22 +0900

    manifest 및 sw.js 추가

---
+++ static/sw.js
@@ -0,0 +1,16 @@
+﻿const CACHE_NAME = 'moment-pick-v1';
+const ASSETS = ['/', '/static/manifest.json', '/static/logo-192.png', '/static/logo-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
